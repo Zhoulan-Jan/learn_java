07ee146ca17047cb1d4f36f8b7987bad47d5eedc
ZhouLan <1832311982@qq.com>
2020-05-15 22:37:44 +0800

    爬虫前端页面

---
+++ projects/crawler/src/main/webapp/projects.sql
@@ -0,0 +1,12 @@
+-- create github_crawler;
+-- use github_crawler;
+-- create table project_table(
+--     name varchar (64),
+--     url varchar (1024),
+--     description varchar (1024),
+--
+--     starCnt int,
+--     forkCnt int,
+--     issueCnt int,
+--     date varchar (128)
+-- )
