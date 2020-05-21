7a0e8115f6c7ac50c1226554e82981e8b9b83655
ZhouLan <1832311982@qq.com>
2020-05-21 22:28:33 +0800

    单例模式 连接数据库

---
+++ advance/jdbc/src/DBUtil.java
@@ -0,0 +1,55 @@
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class DBUtil {
+    private static String URL = "jdbc:mysql://127.0.0.1:3306/github_crawler?characterEncoding=utf-8&useSSL=true";
+    private static String USERNAME = "root";
+    private static String PASSWORD = "123456";
+
+    private static volatile DataSource dataSource = null;
+
+    private static DataSource getDataSource() {
+        if (dataSource == null) {
+            synchronized (DBUtil.class) {
+                if (dataSource == null) {
+//                    dataSource = new MysqlDataSource();
+//                    MysqlDataSource mysqlDataSource = (MysqlDataSource) dataSource;
+//                    mysqlDataSource.setURL(URL);
+//                    mysqlDataSource.setUser(USERNAME);
+//                    mysqlDataSource.setPassword(PASSWORD);
+
+                }
+            }
+        }
+        return dataSource;
+    }
+
+    public static Connection getConnection() {
+        try {
+            return getDataSource().getConnection();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static void close(Connection connection, PreparedStatement preparedStatement, ResultSet resultSet) {
+        try {
+            if (resultSet != null) {
+                resultSet.close();
+            }
+            if (preparedStatement != null) {
+                preparedStatement.close();
+            }
+            if (connection != null) {
+                connection.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
