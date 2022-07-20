c0922e808f10d70fb8f1f1a26c45f170d738121a
ZhouLan <1832311982@qq.com>
2022-07-20 15:37:43 +0800

    链表节点

---
+++ data-structure/list/src/linkedList/internal/ListNode.java
@@ -0,0 +1,10 @@
+package linkedList.internal;
+
+public class ListNode {
+        public int val;
+        public ListNode next;
+
+        public ListNode(int val) {
+            this.val = val;
+        }
+}
